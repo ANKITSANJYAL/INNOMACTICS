d6f4c7eb2e0083b381934e76bd36421522ee8fbf
ANKIT SANJYAL <87311956+ANKITSANJYAL@users.noreply.github.com>
2022-07-25 19:13:53 +0530

    Add files via upload

---
+++ Python Tasks/Task1/print_function.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):   #making a loop to iterate around all the numbers between 1 and the given n
+        print(i, end='')      # end='' parameter in print function will eliminate the enter or print all the numbers in a single line
---
+++ Python Tasks/Task1/write_a_function.py
@@ -0,0 +1,13 @@
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    if((year % 400 == 0) or  (year % 100 != 0) and  (year % 4 == 0)):   
+        leap = True
+    else:  
+        leap = False  
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
